dee32edc2ee009c9e30a442f2e0d3832fc5f983d
anasalbahriii <ama456@case.edu>
2026-09-04 17:22:26 -0400

    Update main.cpp

--- Lab 2/main.cpp
+++ Lab 2/main.cpp
@@ -1,1 +1,19 @@
+//Anas Albahri-(ama456)-Lab2
+#include <Arduino.h>
+#define LED_PIN 13
+
+
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+    Serial.println("Still blinking, still employed.");
+    digitalWrite(LED_PIN, HIGH);
+    delay(300);
+    digitalWrite(LED_PIN, LOW);
+    delay(300);
+}
 
