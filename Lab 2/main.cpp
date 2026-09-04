c7786cd6553b96107a169bd78e1f0c5b54829f8f
anasalbahriii <ama456@case.edu>
2026-09-04 18:05:09 -0400

    Update main.cpp

--- Lab 2/main.cpp
+++ Lab 2/main.cpp
@@ -2,18 +2,17 @@
 #include <Arduino.h>
 #define LED_PIN 13
 
-
-
 void setup() {
   pinMode(LED_PIN, OUTPUT);
+  // ama456: 115200 baud, matches monitor_speed in platformio.ini
   Serial.begin(115200);
 }
 
 void loop() {
+    // ama456: my custom serial message, prints once per blink cycle
     Serial.println("Still blinking, still employed.");
     digitalWrite(LED_PIN, HIGH);
-    delay(300);
+    delay(300);   // ama456: 300 ms on, slow enough to see clearly
     digitalWrite(LED_PIN, LOW);
-    delay(300);
+    delay(300);   // ama456: 300 ms off, same duration for an even blink
 }
-
